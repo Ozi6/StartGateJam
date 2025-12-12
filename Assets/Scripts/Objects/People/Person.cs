6d299bf9dfa7b2c496a7ca4648fda90be3608ecb
Sourchax <de74195@gmail.com>
2025-12-12 21:34:13 +0300

    İyi Nurcan

--- Assets/Scripts/Objects/People/Person.cs
+++ Assets/Scripts/Objects/People/Person.cs
@@ -8,7 +8,9 @@ public abstract class Person : MonoBehaviour
     [SerializeField] protected float attackRange;
     [SerializeField] protected float attackSpeed;
     [SerializeField] protected int aggroVal;
+    [SerializeField] protected float damageArea;
     [SerializeField] protected bool isFriendly;
+    [SerializeField] protected bool[] powerUpList = new bool[5]; // [strength, speed, shield, -, -]
 
     protected abstract void Start();
     protected abstract void Update();
