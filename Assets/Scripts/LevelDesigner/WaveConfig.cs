d0d554079d4d79f1b0caf06b579a775cc881b1ac
Ozi <ozannurcan4@gmail.com>
2025-12-13 21:02:44 +0300

    ORC

--- Assets/Scripts/LevelDesigner/WaveConfig.cs
+++ Assets/Scripts/LevelDesigner/WaveConfig.cs
@@ -3,12 +3,19 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "WaveConfig", menuName = "Wave Configuration", order = 1)]
 public class WaveConfig : ScriptableObject
 {
-    [Header("Spawn Amounts")]
+    [Header("Level 1 Spawn Amounts")]
     [SerializeField][Min(0)] public int giantAmount;
     [SerializeField][Min(0)] public int vikingAmount;
     [SerializeField][Min(0)] public int scoutAmount;
     [SerializeField][Min(0)] public int wizardAmount;
     [SerializeField][Min(0)] public int archerAmount;
+
+    [Header("Level 2 Spawn Amounts")]
+    [SerializeField][Min(0)] public int giantAmountLvl2;
+    [SerializeField][Min(0)] public int vikingAmountLvl2;
+    [SerializeField][Min(0)] public int scoutAmountLvl2;
+    [SerializeField][Min(0)] public int wizardAmountLvl2;
+    [SerializeField][Min(0)] public int archerAmountLvl2;
 
     [Header("Placement Limits")]
     [SerializeField][Min(0)] public int maxPlaceableUnits;
