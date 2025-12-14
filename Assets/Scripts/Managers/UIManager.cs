8e936fd892d815c7527939f7cafccc1ef66d6e94
Sourchax <de74195@gmail.com>
2025-12-14 07:20:19 +0300

    Stats Scroll finalized!

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -22,7 +22,6 @@ public class UIManager : MonoBehaviour
 
     [Header("Upgrade UI (Right Panel)")]
     public GameObject rightSidePanel;
-    public TMP_Text unitStatsText;
     public Button upgradeButton;
     private Vector2 rightPanelClosedPos;
     private Vector2 rightPanelOpenPos;
@@ -110,17 +109,64 @@ public class UIManager : MonoBehaviour
             out bool lifeSteal
         );
 
-        if (unitStatsText != null)
+        if (rightSidePanel != null)
         {
-            unitStatsText.text =
-                $"Unit: {unit.tag}\n" +
-                $"HP: {hp}/{maxHp}\n" +
-                $"Damage: {dmg} (x{dmgMult})\n" +
-                $"Attack Speed: {atkSpd}\n" +
-                $"Range: {atkRange}\n" +
-                $"Move Speed: {moveSpd}";
+            Transform menu = rightSidePanel.transform;
+
+            menu.Find("Stats_Unit").GetComponent<TMP_Text>().text =
+                $"{unit.tag}";
+
+            menu.Find("Stats_Health").GetComponent<TMP_Text>().text =
+                $"{maxHp}";
+
+            menu.Find("Stats_Attack").GetComponent<TMP_Text>().text =
+                $"{dmg}";
+
+            menu.Find("Stats_AttackSpeed").GetComponent<TMP_Text>().text =
+                $"{atkSpd}";
+
+            menu.Find("Stats_Range").GetComponent<TMP_Text>().text =
+                $"{atkRange}";
+
+            menu.Find("Stats_MovementSpeed").GetComponent<TMP_Text>().text =
+                $"{moveSpd}";
+
+            // === UPGRADE COST + ICON ===
+            TMP_Text upgradeCostText =
+                menu.Find("stats_upgrade_cost").GetComponent<TMP_Text>();
+
+            Image upgradeCostIcon =
+                menu.Find("Stats_UpgradeIcon").GetComponent<Image>();
+
+            upgradeCostText.text = $"{person.upgradeCost}";
+
+            // ALREADY UPGRADED → hide cost & icon
+            if (person.upgradeCost == 0)
+            {
+                upgradeCostText.gameObject.SetActive(false);
+                upgradeCostIcon.gameObject.SetActive(false);
+            }
+            else
+            {
+                upgradeCostText.gameObject.SetActive(true);
+                upgradeCostIcon.gameObject.SetActive(true);
+
+                // NOT ENOUGH GOLD → red
+                if (person.upgradeCost > GameManager.Instance.currentGold)
+                {
+                    upgradeCostText.color = Color.red;
+                }
+                else
+                {
+                    // Enough gold → normal color
+                    upgradeCostText.color = Color.black;
+                }
+            }
         }
+
         TMP_Text buttonText = upgradeButton.GetComponentInChildren<TMP_Text>();
+
+        // === BUTTON LOGIC ===
 
         // ALREADY UPGRADED
         if (person.upgradeCost == 0)
@@ -164,6 +210,7 @@ public class UIManager : MonoBehaviour
             );
         }
     }
+
 
     private void UpgradeUnit(GameObject unit)
     {
